8aff3c3aa4326eca88a41e0b1904f48538711b80
leonardodamasceno8 <leonardodamasceno8@users>
2020-04-20 00:45:42 -0300

    #4 done

--- week4/arrayLoopFor/index.js
+++ week4/arrayLoopFor/index.js
@@ -48,3 +48,33 @@ console.log('Exercicio numero 3');
 console.log("");
 trianguloInvertido(5);
 console.log("");
+
+const piramideBase = n => {
+    if (n <= 1) return console.log("Erro, o numero precisa ser maior que 1");
+
+    let string = "";
+    let spacoPar = (n / 2) - 1;
+    let spacoImpar = n / 2;
+    const ast = "*";
+    const space = " ";
+
+    if (n % 2 === 0){
+        // string = space.repeat(spacoPar+0.5) + ast.repeat(1) + space.repeat(spacoPar);
+        // console.log(string);
+        for (let i = 2; i <= n; i+=2){
+            string = space.repeat(spacoPar) + ast.repeat(i) + space.repeat(spacoPar);
+            console.log(string);
+            spacoPar -= 1;
+        }
+    } else {
+        for (let i = 1; i <= n; i+=2){
+            string = space.repeat(spacoImpar) + ast.repeat(i) + space.repeat(spacoImpar);
+            console.log(string);
+            spacoImpar -= 1;
+        }
+    }
+}
+console.log('Exercicio numero 4');
+console.log("");
+piramideBase(8);
+console.log("");
