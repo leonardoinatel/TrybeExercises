76d9b3c1be0b58dad34edd2ddb1be483fa28aaf2
leonardodamasceno8 <leonardodamasceno8@users>
2020-04-21 05:05:22 -0300

    3 exerccc done

--- week4/domSelectors/index.js
+++ week4/domSelectors/index.js
@@ -3,10 +3,21 @@
     - document.getElementById()
     - document.getElementsByClassName()
     - document.getElementsByTagName()
-Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
-Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe.
-Crie uma função que mude a cor do quadrado vermelho para branco.
+
+
 Crie uma função que corrija o texto da tag <h1>.
 Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
 Crie uma função que exiba o conteúdo de todas as tags <p> no console.
-    */
+*/
+
+// Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+const twoYearsLater = document.getElementsByTagName('p')[1];
+twoYearsLater.innerText = "Eu me vejo daqui dois anos em são paulo trabalhando e crescendo profissionalmente";
+
+// Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe.
+const mudaCor = (color) => document.getElementsByClassName('main-content')[0].style.backgroundColor = color;
+mudaCor("#0fa36b");
+
+// Crie uma função que mude a cor do quadrado vermelho para branco.
+const mudaCor2 = (color) => document.getElementsByClassName('center-content')[0].style.backgroundColor = color;
+mudaCor2("white");
