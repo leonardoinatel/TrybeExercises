cade7ac08ff1140848b14ec447d716fb18e1cb6e
leonardodamasceno8 <leonardodamasceno8@users>
2020-04-21 05:23:18 -0300

    aula 4 done

--- week4/domSelectors/index.js
+++ week4/domSelectors/index.js
@@ -3,11 +3,6 @@
     - document.getElementById()
     - document.getElementsByClassName()
     - document.getElementsByTagName()
-
-
-Crie uma função que corrija o texto da tag <h1>.
-Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
-Crie uma função que exiba o conteúdo de todas as tags <p> no console.
 */
 
 // Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
@@ -21,3 +16,24 @@ mudaCor("#0fa36b");
 // Crie uma função que mude a cor do quadrado vermelho para branco.
 const mudaCor2 = (color) => document.getElementsByClassName('center-content')[0].style.backgroundColor = color;
 mudaCor2("white");
+
+// Crie uma função que corrija o texto da tag <h1>. Exercício 4.4 - JavaEscripito
+const mudaTexto = (palavra) => document.getElementsByTagName('h1')[0].innerText = `Exercício 4.4 - ${palavra}`;
+mudaTexto("DOM - Document Object Mananger");
+
+// Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+const upperCase = () => twoYearsLater.innerText = document.getElementsByTagName('p')[1].innerHTML.toUpperCase();
+upperCase();
+
+// Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+const imprime = () => {
+    const tagP = document.getElementsByTagName('p');
+    for (let i = 0; i < tagP.length; i+=1){
+        console.log(tagP[i].innerHTML)
+    }
+
+    tagP.forEach(element => {
+        console.log(element.innerText)
+    });
+}
+imprime();
